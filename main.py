e965e868ed3613fa01cfdbb1fa26d3cc11320b60
Antti Maula <antti.maula@witchcraftlabs.com>
2014-12-14 13:20:25 +0200

    Lisää kohtia

--- main.py
+++ main.py
@@ -4,22 +4,49 @@
 """
 
 ohjelma = {
-    "soita_kavereita": {
-        "tarina": "soitat jokaiselle kaverillesi mutta kaikilla on muita menoja",
-        "vaihtoehdot": [None]
+    "mene_huoneeseen_1": {
+        "tarina": "saavut ensimmäiseen huoneeseen näet siellä tauluja linnan herttuoista",
+        "vaihtoehdot": [("", None),
+                        ("", None),
+                        ("", None)]
+    },
+    "mene_huoneeseen_2": {
+        "tarina": "",
+        "vaihtoehdot": [("", None),
+                        ("", None),
+                        ("", None)]
+    },
+    "mene_huoneeseen_3": {
+        "tarina": "Otat kiinni kolmannen huoneen ovenkahvasta ja vedät varovasti. \n"
+                  "Ovi pistää vastaan mutta aukeaa viimein ilkeästi narahtaen. Edessäsi\n"
+                  "aukeava huone on täysin pimeä etkä näe yhtään eteesi. Huoneessa leijuu \n"
+                  "lievä homeen tuoksu joka viittaa siihen että huone on ollut pitkään \n"
+                  "käyttämättä. Astut varovasti huoneen sisäpuolelle ja tunnet kuinka lattia\n"
+                  "narisee pahaenteisesti jalkojesi alla. Näet huoneen toisessa päässä vaimeaa\n"
+                  "valonkajoa.",
+        "vaihtoehdot": [("Suuntaat varovasti kävellen kohti valonkajoa hapuillen käsilläsi pimeyttä.", ""),
+                        ("", None),
+                        ("Mielestäni huone näyttää liian vaaralliselta ja peräännyt takaisin linnan portille.",
+                         "mene_yksin_tutkimaan_linnaa")]
     },
     "mene_yksin_tutkimaan_linnaa": {
-        "tarina": "menet yksin tutkimaan linnaa ja huomaat kolme ränsistynyttä ovea jotka viävät eri huoneisiin",
-        "vaihtoehdot": [("mene huoneeseen 1", None),
-                        ("mene huoneeseen 2", None),
-                        ("mene huoneeseen 3", None)]
+        "tarina": "menet yksin tutkimaan linnaa ja huomaat kolme ränsistynyttä ovea\n"
+                  "jotka vievät eri huoneisiin.",
+        "vaihtoehdot": [("mene huoneeseen 1", "mene_huoneeseen_1"),
+                        ("mene huoneeseen 2", "mene_huoneeseen_2"),
+                        ("mene huoneeseen 3", "mene_huoneeseen_3")]
+    },
+    "soita_kavereita": {
+        "tarina": "soitat jokaiselle kaverillesi mutta kaikilla on muita menoja.\n"
+                  "Päätät että et mene linnaan ilman kavereita ja lähdet kotiin.",
+        "vaihtoehdot": None
     },
     "soita_isasi_mukaan": {
-        "tarina": "Otat puhelimen taskustasi ja soitat isällesi. Isäsi vastaa puhelimeen. "
-                  "Kerrot että näit kukkulan laella kiinnostavan näköisen linnan ja "
+        "tarina": "Otat puhelimen taskustasi ja soitat isällesi. Isäsi vastaa puhelimeen.\n"
+                  "Kerrot että näit kukkulan laella kiinnostavan näköisen linnan ja\n"
                   "haluaisit mennä tutkimaan sitä. Odotat innostuneena, mutta isä vastaakin että:\n'"
                   "Hylätty linna? Liian hurjaa! Et saa mennä! Nyt heti kotiin!'\n"
-                  "Mutiset vastaan mutta toeat että ehkä on parempi olla menemättä yksin "
+                  "Mutiset vastaan mutta toeat että ehkä on parempi olla menemättä yksin\n"
                   "epäilyttävän näköiseen linnaan.",
         "vaihtoehdot": None
     },
@@ -44,18 +71,24 @@ def main():
             if vaihtoehdot is None:
                 print("Peli loppui!")
                 break
-            print("\nVaihtoehdot:")
-            for index, vaihtoehto in enumerate(vaihtoehdot):
-                kirjain = vaihtoehtokirjaimet[index]
-                print("%s: %s" % (kirjain, vaihtoehto[0]))
-            input = raw_input("Mitä haluat tehdä? ")
-            try:
-                newindex = vaihtoehtokirjaimet.index(input)
-            except ValueError:
-                print("Tuntematon syöte, valitse joku arvoista: %s" % ', '.join(vaihtoehtokirjaimet))
-                continue
 
-            tila = ohjelma[vaihtoehdot[newindex][1]]
+            elif isinstance(vaihtoehdot, basestring):
+                print("\nSiirryt suoraan uuteen tilanteeseen:")
+                tila = ohjelma[vaihtoehdot]
+
+            else:
+                print("\nVaihtoehdot:")
+                for index, vaihtoehto in enumerate(vaihtoehdot):
+                    kirjain = vaihtoehtokirjaimet[index]
+                    print("%s: %s" % (kirjain, vaihtoehto[0]))
+                input = raw_input("Mitä haluat tehdä? ")
+                try:
+                    newindex = vaihtoehtokirjaimet.index(input)
+                except ValueError:
+                    print("Tuntematon syöte, valitse joku arvoista: %s" % ', '.join(vaihtoehtokirjaimet))
+                    continue
+
+                tila = ohjelma[vaihtoehdot[newindex][1]]
 
     except KeyboardInterrupt:
         print("\n\nKeskeytit pelin.\n")
